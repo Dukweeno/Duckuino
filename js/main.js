28f1d7f4095f2dde5e0ab506d68df3340379d5fd
Daniel Shlomo <dshlomo4@gmail.com>
2016-09-30 00:15:55 +0300

    fixed JSZip

--- js/main.js
+++ js/main.js
@@ -27,30 +27,21 @@ $(function() { // Wait for jQuery
   });
 });
 
+
+
 // Download button
 $("#download").click(function() {
-	var payloadValue = editor.getValue();
-	var payloadName = $("#payloadName").val();
-	if(payloadValue == undefined || payloadValue == '' || payloadValue == 'Error, look at the console console...' || payloadName == '' || payloadName == undefined){
-		alert("Download Error: The payload or payload name are empty!");
-		return;
-	}
-  // create `a` element
-  $("<a />", {
-      // if supported , set name of file
-      download: payloadName + ".ino",
-      // set `href` to `objectURL` of `Blob` of `textarea` value
-      href: URL.createObjectURL(
-        new Blob([editor.getValue()], {
-          type: "text/plain"
-        }))
-    })
-    // append `a` element to `body`
-    // call `click` on `DOM` element `a`
-    .appendTo("body")[0].click();
-    // remove appended `a` element after "Save File" dialog,
-    // `window` regains `focus` 
-    $(window).one("focus", function() {
-      $("a").last().remove()
-    })
+  var payloadValue = editor.getValue();
+  var payloadName = $("#payloadName").val();
+  if(payloadValue == undefined || payloadValue == '' || payloadValue == 'Error, look at the console console...' || payloadName == '' || payloadName == undefined){
+    alert("Download Error: The payload or payload name are empty!");
+    return;
+  }
+  var zip = new JSZip();
+
+  zip.file(payloadName + ".ino", payloadValue);
+
+  zip.generateAsync({type:"blob"}).then(function(content) {
+      saveAs(content, ".zip");
+  });
 })
