c28f0f0650bb312c28b04bb9d878e81f780ad31a
Leon ROUX <contact@heofon.co>
2019-04-20 18:02:52 +0200

    Fixed responsiveness of the line number addon

--- assets/js/lib/jquery-linenumbers.js
+++ assets/js/lib/jquery-linenumbers.js
@@ -21,80 +21,98 @@ function getCharByRow(e) {
   return chars - 2;
 }
 
+function update(ev, self, opts, linebox) {
+  /* Interpret keydown as change */
+  if (ev.type == 'keydown') {
+    setTimeout(() => $(self).trigger('change'), 0);
+    return;
+  }
+
+  /* Make sure height is right */
+  $(linebox).css('height',
+    ($(self).height()
+    + parseInt($(self).css('padding-top'))
+    + parseInt($(self).css('padding-bottom'))) + 'px');
+
+  /* Get the number of printable char in line */
+  let charbr = getCharByRow($(self));
+
+  // Split value into lines
+  let lines = $(self).val().split('\n');
+  // declare output var
+  let output='';
+  // declare spacers and max_spacers vars, and set defaults
+  let max_spacers = '';
+  for(i=0; i < opts.digits; i++)
+    max_spacers += ' ';
+  // Loop through and process each line
+  $.each(lines, function(k, v) {
+    // Add a line if not blank
+    if (k != 0)
+      output += '\n';
+    // Determine the appropriate number of leading spaces
+    let lencheck = k + opts.start + '!';
+    let spacers = max_spacers.substr(lencheck.length - 1);
+    // Add the line with out line number, to the output variable
+    if (k >= Math.pow(10, opts.digits) - 1)
+      output += "\n"
+    else {
+      output += spacers + (k + opts.start) + ':';
+      if (v != "")
+        output += "\n--".repeat((v.length - 1) / charbr);
+      }
+  });
+
+  // Give the text area out modified content.
+  $(linebox).val(output);
+  // Change scroll position as they type, makes sure they stay in sync
+  $(linebox).scrollTop($(self).scrollTop());
+
+}
+
+/* On JQuery ready */
 (function($) {
   $.fn.linenumbers = function(inopts){
     let opts = $.extend({
-      width: 32,
-      padding: 8,
+      width: 42,
       start: 1,
-      digits: 4
+      digits: 3
     }, inopts);
     $('[data-name="linenumbers"]').remove();
 
-    return this.each(function(){
+    return this.each(function() {
       let self = this;
-      let width = $(this).width();
-      let height = $(this).height();
-      let newwidth = width - opts.width;
 
-      $(this).before('<textarea                  \
-          data-name="linenumbers"                \
-          style="                                \
-              width: ' + newwidth + 'px;         \
-              height: ' + height + 'px;          \
-              float: left;                       \
-              margin-right: -' + newwidth + 'px; \
-              white-space: pre; overflow: hidden;\
-          " disabled></textarea>');
-      $(this).css({'width': newwidth + 'px', 'height': height + 'px',
-        'padding': opts.padding + 'px', 'float': 'right', 'overflow-y': 'hidden'});
-      $(this).after('<div style="clear: both;"></div>');
+      /* Generate the line number text-area */
+      $(this).before('<textarea data-name="linenumbers" disabled></textarea>');
 
-      let lnbox = $(this).parent().find('textarea[data-name="linenumbers"]');
-      let charbr = getCharByRow($(this));
+      /* Get the generated text-area and style it */
+      let linebox = $(self).parent().find('textarea[data-name="linenumbers"]');
+      $(linebox).css('width', opts.width + 'px');
+      $(linebox).css('height',
+        ($(this).height()
+        + parseInt($(this).css('padding-top'))
+        + parseInt($(this).css('padding-bottom'))) + 'px');
+      $(linebox).css('padding-right', '0');
+      $(linebox).css('position', 'absolute');
+      $(linebox).css('overflow', 'hidden');
+      $(linebox).css('border-top-right-radius', '0');
+      $(linebox).css('border-bottom-right-radius', '0');
 
-      $(this).on('keydown keyup change', function(ev) {
-        // Make change directly
-        if (ev.type == 'keydown') {
-          setTimeout(() => $(this).trigger('change'), 0);
-          return;
-        }
+      $(this).css({'padding-left':
+        (opts.width + parseInt($(self).css('padding-left'))) + 'px',
+        'overflow': 'hidden'});
 
-        // Split value into lines
-        let lines = $(this).val().split('\n');
-        // declare output var
-        let output='';
-        // declare spacers and max_spacers vars, and set defaults
-        let max_spacers = '';
-        for(i=0; i < opts.digits; i++)
-          max_spacers += ' ';
-        // Loop through and process each line
-        $.each(lines,function(k, v) {
-          // Add a line if not blank
-          if (k != 0)
-            output += '\n';
-          // Determine the appropriate number of leading spaces
-          let lencheck = k + opts.start + '!';
-          let spacers = max_spacers.substr(lencheck.length - 1);
-          // Add the line with out line number, to the output variable
-          if (k >= Math.pow(10, opts.digits) - 1)
-            output += "\n"
-          else {
-            output += spacers + (k + opts.start) + ':';
-            if (v != "")
-              output += "\n---".repeat((v.length - 1) / charbr);
-          }
-        });
-        // Give the text area out modified content.
-        $(lnbox).val(output);
-        // Change scroll position as they type, makes sure they stay in sync
-        $(lnbox).scrollTop($(this).scrollTop());
+      /* Update on events */
+      $(this).on('keydown keyup change', (ev) => update(ev, self, opts, linebox));
+      $(window).resize((ev) => update(ev, self, opts, linebox));
+
+      /* Scroll with the base text-area */
+      $(this).scroll(function() {
+        $(linebox).scrollTop($(this).scrollTop());
       });
-      // Lock scrolling together, for mouse-wheel scrolling
-      $(this).scroll(function(){
-        $(lnbox).scrollTop($(this).scrollTop());
-      });
-      // Fire it off once to get things started
+
+      /* Fire it off once to get things started */
       $(this).trigger('change');
     });
   };
